ceb1b72702d8b2c1987cc70d8cd23fbd33d1eae3
Bastien Limbour Pruvost <bastien.limbour.dev@gmail.com>
2026-06-16 15:21:19 +0200

    feat: standardize component styles by updating border radius and background colors in ProjectTechno and SkillTechno components

--- src/components-ui/project-techno.tsx
+++ src/components-ui/project-techno.tsx
@@ -9,13 +9,7 @@ type ProjectTechnoProps = {
 
 export const ProjectTechno = ({ techno }: ProjectTechnoProps) => {
   return (
-    <div
-      className={cn(
-        'rounded-full px-2 py-1 shadow-sm',
-        'flex items-center gap-2',
-        'border border-zinc-100 bg-white dark:border-zinc-600 dark:bg-zinc-700',
-      )}
-    >
+    <div className='flex items-center gap-2 rounded-full border border-zinc-100 bg-zinc-50 px-2 py-1 shadow-sm dark:border-zinc-600 dark:bg-zinc-700'>
       <DynamicIcon iconName={techno.iconName} label={`Logo ${techno.title}`} className='h-4 w-4' />
       <p className={cn('text-xs font-normal')}>{techno.title}</p>
     </div>
--- src/components-ui/skill-techno.tsx
+++ src/components-ui/skill-techno.tsx
@@ -8,7 +8,7 @@ type SkillTechnoProps = {
 
 export const SkillTechno = ({ techno }: SkillTechnoProps) => {
   return (
-    <div className='inline-flex items-center gap-2 rounded-md border border-zinc-50 bg-zinc-50 px-3 py-1.5 shadow-sm dark:border-zinc-600 dark:bg-zinc-700'>
+    <div className='inline-flex items-center gap-2 rounded-full border border-zinc-100 bg-zinc-50 px-3 py-1.5 shadow-sm dark:border-zinc-600 dark:bg-zinc-700'>
       <DynamicIcon iconName={techno.iconName} label={`Logo ${techno.title}`} className='size-5' />
       <p className='text-xs font-medium text-zinc-700 dark:text-zinc-200'>{techno.title}</p>
     </div>
